e326c2cd6af2cd12eb7378ed073163b6952e6f3c
Michael Lackey <michael.g.lackey@gmail.com>
2020-04-16 22:42:38 -0500

    add colorpicker (unworking)

--- src/components/AddPaint/AddPaint.jsx
+++ src/components/AddPaint/AddPaint.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { SketchPicker } from 'react-color';
+import reactCSS from 'reactcss';
 import './AddPaint.css';
 
 class AddPaint extends Component {
@@ -6,31 +8,87 @@ class AddPaint extends Component {
         formData: {
             colorName: '',
             paintType: '',
-            isOwned: false
+            isOwned: false,
+        },
+        displayColorPicker: false,
+        color: {
+            r: '',
+            g: '',
+            b: '',
+            a: ''
         }
-    }
+    };
 
-    handleChange = e => {
+    handleChange = (e) => {
         if(e.target.name === 'isOwned'){
             e.target.value = !this.state.formData.isOwned
         }this.setState({
-            formData: {...this.state.formData, [e.target.name]:e.target.value}
+            formData: {...this.state.formData,[e.target.name]:e.target.value}
         })
     }
 
-    handleSubmit = e => {
+    handleSubmit = (e) => {
         e.preventDefault()
         this.props.handleAddPaint(this.state.formData)
     }
 
+    handleClick = () => {
+        this.setState({ 
+            displayColorPicker: !this.state.displayColorPicker
+        })
+      };
+    
+    handleClose = () => {
+        this.setState({
+            displayColorPicker: false
+        })
+    };
+
+    handleColorChange = (color) => {
+        this.setState({
+            color: color.rgb
+        })
+    };
+    
+
     render() {
+        const styles = reactCSS({
+            'default': {
+              color: {
+                width: '36px',
+                height: '14px',
+                borderRadius: '2px',
+                background: `rgba(${ this.state.color.r }, ${ this.state.color.g }, ${ this.state.color.b }, ${ this.state.color.a })`,
+              },
+              swatch: {
+                padding: '5px',
+                background: '#fff',
+                borderRadius: '1px',
+                boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+                display: 'inline-block',
+                cursor: 'pointer',
+              },
+              popover: {
+                position: 'absolute',
+                zIndex: '2',
+              },
+              cover: {
+                position: 'fixed',
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+                left: '0px',
+              },
+            },
+          });
+
         return (
             <form
                 className="form"
                 onSubmit={this.handleSubmit}
             >
-                <span>Paint Color</span>
-                <br></br>
+                <span>Paint Name</span>
+                <br></br><br></br>
                 <input
                     className="input-field"
                     type="text"
@@ -39,14 +97,41 @@ class AddPaint extends Component {
                     onChange={this.handleChange}
                     autoComplete="off"
                 />
+                <br></br><br></br><br></br>
+                <span>Color</span>
                 <br></br><br></br>
-                <span>Paint Type</span>
-                <br></br>
+                <div>
+                    <div
+                        style={ styles.swatch }
+                        onClick={ this.handleClick }
+                    >
+                    <div 
+                        style={ styles.color }
+                    />
+                    </div>
+                    { this.state.displayColorPicker ? 
+                        <div
+                            style={ styles.popover }
+                        >
+                        <div 
+                            style={ styles.cover }
+                            onClick={ this.handleClose }
+                        />
+                        <SketchPicker
+                            color={ this.state.color } onChange={ this.handleColorChange }
+                        />
+                        </div> 
+                    : 
+                        null
+                    }
+                </div>
+                <br></br><br></br><br></br>
+                <span>Type</span>
+                <br></br><br></br>
                 <select
                     className="input-field"
                     type="text"
                     name="paintType"
-                    value={this.state.formData.paintType}
                     onChange={this.handleChange}
                 >
                     <option>-</option>
@@ -57,9 +142,8 @@ class AddPaint extends Component {
                     <option>Glaze</option>
                     <option>Technical</option>
                 </select>
-                <br></br><br></br>
-                <span>In Collection?</span>
-                <br></br>
+                <br></br><br></br><br></br>
+                <span>In Collection?</span>&nbsp;&nbsp;
                 <input
                     type="checkbox"
                     name="isOwned"
--- src/pages/UpdatePaintPage/UpdatePaintPage.jsx
+++ src/pages/UpdatePaintPage/UpdatePaintPage.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import { SwatchesPicker } from 'react-color';
+import './UpdatePaintPage.css';
 
 class UpdatePaintPage extends Component {
   state = {
@@ -29,47 +30,78 @@ class UpdatePaintPage extends Component {
   render() {
     return (
       <>
-        <h3>Update Your Paint</h3>
-        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>Paint Color</label>
-            <input
-                className="form-control"
-                name="colorName"
-                value={this.state.formData.colorName}
-                onChange={this.handleChange}
-                required
-            />
-          </div>
-          <div className="form-group">
-            <label>Paint Type</label>
-            <input
-                className="form-control"
+        <h3>Update "{this.state.formData.colorName}"</h3>
+        <center>
+          <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
+          <span>Paint Name</span>
+                <br></br><br></br>
+                <input
+                    className="input-field"
+                    type="text"
+                    name="colorName"
+                    value={this.state.formData.colorName}
+                    onChange={this.handleChange}
+                    autoComplete="off"
+                />
+                <br></br><br></br><br></br>
+            <span>Color</span>
+            <br></br><br></br>
+              <SwatchesPicker
+                color= {this.state.color}
+                onChangeComplete = {this.handleChangeComplete}
+                width = {220}
+                height = {150}
+              />
+            <br></br><br></br><br></br>
+            <label>Type</label>
+            <br></br><br></br>
+            <select
+                className="input-field"
+                type="text"
                 name="paintType"
                 value={this.state.formData.paintType}
                 onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label>In Your Collection?</label>
+            >
+                <option>-</option>
+                <option>Base</option>
+                <option>Shade</option>
+                <option>Layer</option>
+                <option>Edge</option>
+                <option>Glaze</option>
+                <option>Technical</option>
+            </select>
+            <br></br><br></br><br></br>
+            <label>In Collection?</label>
+            &nbsp;&nbsp;
             <input
-                type="checkbox"
-                className="form-control"
-                name="isOwned"
-                checked={this.state.formData.isOwned}
-                value={this.state.formData.isOwned}
-                onChange={this.handleChange}
+              type="checkbox"
+              className="form-control"
+              name="isOwned"
+              checked={this.state.formData.isOwned}
+              value={this.state.formData.isOwned}
+              onChange={this.handleChange}
             />
-          </div>
-          <button
-                type="submit"
-                className="btn"
-                disabled={this.state.invalidForm}
+            <br></br><br></br><br></br>
+            <button
+              type="submit"
+              className="btn"
+              disabled={this.state.invalidForm}
+            >
+              SAVE
+            </button>
+          </form>
+          <br></br>
+          <form
+            action="/paintlist"
           >
-            SAVE
-          </button>&nbsp;&nbsp;
-          <Link to='/paintlist'>CANCEL</Link>
-        </form>
+            <input
+              type="submit"
+              value="CANCEL"
+              className="btn"
+            >
+            </input>
+          </form>
+        </center>
       </>
     );
   }
