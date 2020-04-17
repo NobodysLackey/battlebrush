f8da6e1bd0dbfc2e180514129a38ed4edcc66e82
Michael Lackey <michael.g.lackey@gmail.com>
2020-04-17 01:20:37 -0500

    add rainbow styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,26 +4,35 @@ import './NavBar.css';
 
 const NavBar = (props) => {
   let nav = props.user ?
-    <div>
-      <Link to='/' className='NavBar-link'>Home</Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to='/paintlist' className='NavBar-link'>Paints</Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to='/addpaint' className='NavBar-link'>Add</Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to='/profile' className='NavBar-link'>Profile</Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to='' className='NavBar-link' onClick={props.handleLogout}>Logout</Link>
+    <div className='NavBar'>
+      <div className="logo">
+        <img className="navlogo" src="../logo192.png" alt="logo"></img>
+      </div>
+      <div className="links">
+        <Link to='/' className='NavBar-link'>Home</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/paintlist' className='NavBar-link'>Paints</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/addpaint' className='NavBar-link'>Add</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/profile' className='NavBar-link'>Profile</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='' className='NavBar-link' onClick={props.handleLogout}>Logout</Link>
+      </div>
     </div>
     :
-    <div>
-      <Link to='/login' className='NavBar-link'>Login</Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to='/signup' className='NavBar-link'>Sign up</Link>
-    </div>;
-
-  return (
     <div className='NavBar'>
+      <div className="logo">
+        <img src="./logo192.png" alt="logo"></img>
+      </div>
+      <div className="links">
+        <Link to='/login' className='NavBar-link'>Login</Link>
+        &nbsp;&nbsp;|&nbsp;&nbsp;
+        <Link to='/signup' className='NavBar-link'>Sign up</Link>
+      </div>;
+    </div>
+  return (
+    <div className="nav-container">
       {nav}
     </div>
   );
