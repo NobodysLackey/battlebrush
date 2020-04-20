057dc627c88870ea2de6d12e496811f72efe9826
Michael Lackey <michael.g.lackey@gmail.com>
2020-04-20 15:59:16 -0500

    fix auth issue

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ const SECRET = process.env.SECRET;
 
 module.exports = (req, res, next) => {
   let token = req.get('Authorization') || req.query.token || req.body.token;
-  if(token == null) {
+  if(token) {
     token = token.replace('Bearer ', '');
     jwt.verify(token, SECRET, (err, decoded) => {
       if (err) {
