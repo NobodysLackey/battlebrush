7e10b20aba99f22412fd238324fcb196353ec575
Michael Lackey <michael.g.lackey@gmail.com>
2020-11-11 17:57:31 -0600

    remove console.log

--- src/pages/PaintListPage/PaintListPage.jsx
+++ src/pages/PaintListPage/PaintListPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PaintList from '../../components/PaintList/PaintList';
 
 const PaintListPage = (props) => {
-  console.log(props)
   return (
     <PaintList 
       paints = {props.paints}
